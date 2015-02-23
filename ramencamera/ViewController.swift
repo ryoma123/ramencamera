5444bb9eb1523a6af55edd5f8ebca63c7198f409
Ryoma Ono <ryoma.ono.2661@gmail.com>
2015-02-24 08:55:26 +0900

    ラーメンカメラつくるよ

---
+++ ramencamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ramencamera
+//
+//  Created by usr0600328 on 2015/02/24.
+//  Copyright (c) 2015年 ryoma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
